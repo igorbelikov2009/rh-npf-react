e9858f42ee266b8c382c08a64bbbe0372d6d0dc5
igor <igorbelikov2009@yandex.ru>
2023-01-21 20:03:35 +0300

    topMenu phone

--- src/components/header/topMenu/TopMenu.tsx
+++ src/components/header/topMenu/TopMenu.tsx
@@ -12,71 +12,62 @@ const TopMenu: FC = () => {
   return (
     <header className={isBackgroundWhite ? "top-menu top-menu_news" : "top-menu "}>
       <div className="top-menu__container">
-        <div className="top-menu__row">
-          <div className="top-menu__left-block">
-            <div className="top-menu__button-lines">
-              <img src={Hamburger} alt="Hamburger" className="triple-icon" />
-            </div>
+        <div className="top-menu__left-block">
+          <div className="top-menu__icon-hamburger">
+            <img src={Hamburger} alt="Hamburger" className="triple-icon" />
+          </div>
 
-            <div className="top-menu__logotype">
-              <Logotypes isBackgroundWhite={isBackgroundWhite} />
-            </div>
+          <div className="top-menu__logotype">
+            <Logotypes isBackgroundWhite={isBackgroundWhite} />
+          </div>
 
-            <div className="top-menu__nav">
-              <Link className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"} to="/">
-                Главная
-              </Link>
-              <Link
-                className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"}
-                to="/about"
-              >
-                О нас
-              </Link>
-              <Link
-                className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"}
-                to="/business"
-              >
-                Для бизнеса
-              </Link>
-              <Link
-                className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"}
-                to="/contacts"
-              >
-                Контакты
-              </Link>
-              <Link className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"} to="/news">
-                Новости
-              </Link>
-              <Link
-                className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"}
-                to="/investment"
-              >
-                Инвестиции
-              </Link>
+          <div className="top-menu__nav">
+            <Link className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"} to="/">
+              Главная
+            </Link>
+            <Link className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"} to="/about">
+              О нас
+            </Link>
+            <Link
+              className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"}
+              to="/business"
+            >
+              Для бизнеса
+            </Link>
+            <Link
+              className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"}
+              to="/contacts"
+            >
+              Контакты
+            </Link>
+            <Link className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"} to="/news">
+              Новости
+            </Link>
+            <Link
+              className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"}
+              to="/investment"
+            >
+              Инвестиции
+            </Link>
+          </div>
+        </div>
+
+        <div className="top-menu__right-block">
+          <div className="top-menu__contacts">
+            <div className="top-menu__phone">
+              <a className="top-menu__phone-link" href="tel:+78002004766">
+                <div className={isBackgroundWhite ? "top-menu__link  top-menu__link_news" : "top-menu__link"}>
+                  8 800 200-47-66
+                </div>
+              </a>
             </div>
           </div>
 
-          <div className="top-menu__right-block">
-            <div className="top-menu__contacts">
-              <div className="top-menu__phone">
-                <a className="top-menu__phone-link" href="tel:+78002004766">
-                  <div
-                    className={
-                      isBackgroundWhite ? "top-menu__phone-number  top-menu__title_news" : "top-menu__phone-number"
-                    }
-                  >
-                    8 800 200-47-66
-                  </div>
-                </a>
-              </div>
-            </div>
+          <div className="top-menu__private-office">
+            <img src={User} alt="Hamburger" className="triple-icon" />
 
-            <div className="top-menu__private-office">
-              <img src={User} alt="Hamburger" className="triple-icon" />
-
-              <div className={isBackgroundWhite ? "top-menu__title  top-menu__title_news" : "top-menu__title"}>
-                Личный кабинет
-              </div>
+            <div className={isBackgroundWhite ? "top-menu__title  top-menu__title_news" : "top-menu__title"}>
+              Личный кабинет
             </div>
           </div>
         </div>
