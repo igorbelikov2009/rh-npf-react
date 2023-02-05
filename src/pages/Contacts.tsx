dd3314f9c30fb19eb26a1cca9b8cdd4ccb64835e
igor <igorbelikov2009@yandex.ru>
2023-02-05 11:46:47 +0300

    Contacts page

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from "react";
+import Address from "../components/contacts/Address/Address";
+import ContactsMap from "../components/contacts/ContactsMap/ContactsMap";
+import "../styles/dist/Contacts.css";
 
 const Contacts: FC = () => {
-  return <div>Contacts</div>;
+  return (
+    <div className="contacts">
+      <Address />
+      <ContactsMap />
+    </div>
+  );
 };
 
 export default Contacts;
